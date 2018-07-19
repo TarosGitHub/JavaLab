10af60710deaca6beeb3dae4a190777be6c5a725
Taro Hoshino <no_taro1990@outlook.com>
2018-07-20 00:19:12 +0900

    Add XmlReaderTest

---
+++ XML_File_Proc/test/xmlproc/XmlReaderTest.java
@@ -0,0 +1,14 @@
+package xmlproc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class XmlReaderTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
