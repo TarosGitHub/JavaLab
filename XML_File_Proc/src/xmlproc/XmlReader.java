111e6d597718943a6b603424212633cfff8497dd
Taro Hoshino <no_taro1990@outlook.com>
2018-09-09 17:25:41 +0900

    XML読み込みテスト

--- XML_File_Proc/src/xmlproc/XmlReader.java
+++ XML_File_Proc/src/xmlproc/XmlReader.java
@@ -1,6 +1,7 @@
 package xmlproc;
 
 import java.io.IOException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -19,7 +20,7 @@ public class XmlReader {
 	/**
 	 * Specifies the path to the file and reads the XML file. Returns the Document
 	 * object when the file is successfully read.
-	 * 
+	 *
 	 * @param file The path to the file. If you pass {@code null} as this argument,
 	 *             IOException will be raised.
 	 * @return Document object of the read XML file. It does not return null.
@@ -29,8 +30,8 @@ public class XmlReader {
 	 */
 	public static Document read(String file) throws IOException, SAXException, ParserConfigurationException {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder documentBuilder = factory.newDocumentBuilder();
-		return documentBuilder.parse(file);
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		return builder.parse(file);
 	}
 
 }
--- XML_File_Proc/test/xmlproc/XmlReaderTest.java
+++ XML_File_Proc/test/xmlproc/XmlReaderTest.java
@@ -1,14 +1,39 @@
 package xmlproc;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 class XmlReaderTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testRead() {
+
+		try {
+			Document doc = XmlReader.read("testdata/xmlproc/XmlReaderTest_testRead.xml");
+
+			Element top = doc.getDocumentElement();
+//			NodeList nodes = top.getChildNodes();
+//			for (int i = 0; i < nodes.getLength(); i++) {
+//				Node node = nodes.item(i);
+//				String nodeName = node.getNodeName();
+//
+//				if ("first".equals(nodeName)) {
+//
+//				}
+//			}
+
+			assertThat(top.getTagName(), is("top"));
+
+		} catch (Exception e) {
+			fail("テストに失敗");
+		}
 	}
 
 }
